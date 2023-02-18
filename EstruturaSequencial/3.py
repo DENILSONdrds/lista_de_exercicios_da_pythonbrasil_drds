1750d0390c6fe3f650f08b439c605352285403b6
denis <denis_snp@hotmail.com>
2023-02-18 06:54:50 -0400

    Resolução de alguns exercicios.

---
+++ EstruturaSequencial/3.py
@@ -0,0 +1,6 @@
+""" 3. Faça um Programa que peça dois números e imprima a soma."""
+
+número_1 = int(input('Digite um número: '))
+número_2 = int(input('Digite outro número: '))
+soma = número_1 + número_2
+print(f'A soma dos números é : {soma}')
