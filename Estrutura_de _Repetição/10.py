b0fbe0fc4d9075439881eb1a9ab9abcc1f94a2f1
denis <denis_snp@hotmail.com>
2023-02-23 22:32:09 -0400

    Resolução dos exercicios..

--- Estrutura_de _Repetição/10.py
+++ Estrutura_de _Repetição/10.py
@@ -4,15 +4,17 @@
 n_1 = int(input('Digite um número: '))
 n_2 = int(input('Digite outro número: '))
 
-if n_1 < n_2:
+if n_1 < n_2 and n_2 - n_1 != 1:
     print(f'Os números entre o intervalo de : {n_1} e {n_2} são :')
-    for n in range(n_1, n_2):
+    for n in range(n_1+1, n_2):
         print(n, end=', ')
 
-elif n_1 > n_2:
+
+elif n_1 > n_2 and n_1 - n_2 != 1:
     print(f'Os números entre o intervalo de : {n_2} e {n_1} são :')
-    for n in range(n_2, n_1):
+    for n in range(n_2+1, n_1):
         print(n, end=', ')
+
 
 else:
-    print('Os dois números são iguais, digite números diferentes.')
+    print('Os dois números são iguais ou não existe números entre eles, digite números diferentes.')
--- Estrutura_de _Repetição/11.py
+++ Estrutura_de _Repetição/11.py
@@ -3,18 +3,22 @@
 
 n_1 = int(input('Digite um número: '))
 n_2 = int(input('Digite outro número: '))
-numeros_intervalo = ()
-if n_1 < n_2:
+soma = 0
+
+if n_1 < n_2 and n_2 - n_1 != 1:
     print(f'Os números entre o intervalo de : {n_1} e {n_2} são :')
-    for n in range(n_1, n_2):
+    for n in range(n_1+1, n_2):
         print(n, end=', ')
+        soma += n
+    print(f'E a soma deles é : {soma}.')
 
-elif n_1 > n_2:
+elif n_1 > n_2 and n_1 - n_2 != 1:
     print(f'Os números entre o intervalo de : {n_2} e {n_1} são :')
-    for n in range(n_2, n_1):
+    for n in range(n_2+1, n_1):
         print(n, end=', ')
-
+        soma += n
+    print(f'E a soma deles é : {soma}.')
 
 else:
-    print('Os dois números são iguais, digite números diferentes.')
+    print('Os dois números são iguais ou não existe números entre eles, digite números diferentes.')
 
