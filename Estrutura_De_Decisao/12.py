ad16a8f4d53eb24f3a7c8f4d8b0c12e12c3d9112
denis <denis_snp@hotmail.com>
2023-03-09 16:35:18 -0400

    Resolução dos exercicios..

--- Estrutura_De_Decisao/12.py
+++ Estrutura_De_Decisao/12.py
@@ -9,3 +9,28 @@ o percentual de aumento aplicado;
 o valor do aumento;
 o novo salário, após o aumento.
 """
+
+
+def aumento_salarial():
+    salario_inicial = float(input('Informe o salario: '))
+    if salario_inicial <= 280:
+        aumento = 1.2
+    elif 280 < salario_inicial <= 700:
+        aumento = 1.15
+    elif 700 < salario_inicial <= 1500:
+        aumento = 1.1
+    else:
+        aumento = 1.05
+
+    salario_com_aumento = aumento * salario_inicial
+    percentual_aumento = (aumento / 1.0 - 1) * 100
+    valor_aumento = salario_com_aumento - salario_inicial
+
+    print(f'Salario inicial: {salario_inicial:.2f} R$\n'
+          f'Percentual de aumento aplicado: {percentual_aumento:.2f} %\n'
+          f'Valor do aumento: {valor_aumento:.2f} R$ \n'
+          f'Novo Salário: {salario_com_aumento:.2f} R$ ')
+
+
+if __name__ == '__main__':
+    aumento_salarial()
