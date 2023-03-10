5ac11e4a10dc0e9ce22c775ebeacc4b8257d7757
denis <denis_snp@hotmail.com>
2023-03-09 23:29:13 -0400

    Resolução dos exercicios..

--- Estrutura_De_Decisao/12.py
+++ Estrutura_De_Decisao/12.py
@@ -8,11 +8,39 @@ o salário antes do reajuste;
 o percentual de aumento aplicado;
 o valor do aumento;
 o novo salário, após o aumento.
+
+def aumento_salarial():
+
+    >>> salario_inicial = 250.0
+    >>> aumento_salarial(salario_inicial)
+    Salario inicial: 250.00 R$
+    Percentual de aumento aplicado: 20.00 %
+    Valor do aumento: 50.00 R$
+    Novo Salário: 300.00 R$
+    >>> salario_inicial = 500.0
+    >>> aumento_salarial(salario_inicial)
+    Salario inicial: 500.00 R$
+    Percentual de aumento aplicado: 15.00 %
+    Valor do aumento: 75.00 R$
+    Novo Salário: 575.00 R$
+    >>> salario_inicial = 1000.0
+    >>> aumento_salarial(salario_inicial)
+    Salario inicial: 1000.00 R$
+    Percentual de aumento aplicado: 10.00 %
+    Valor do aumento: 100.00 R$
+    Novo Salário: 1100.00 R$
+    >>> salario_inicial = 2000.0
+    >>> aumento_salarial(salario_inicial)
+    Salario inicial: 2000.00 R$
+    Percentual de aumento aplicado: 5.00 %
+    Valor do aumento: 100.00 R$
+    Novo Salário: 2100.00 R$
+
+
 """
 
 
-def aumento_salarial():
-    salario_inicial = float(input('Informe o salario: '))
+def aumento_salarial(salario_inicial):
     if salario_inicial <= 280:
         aumento = 1.2
     elif 280 < salario_inicial <= 700:
@@ -28,9 +56,8 @@ def aumento_salarial():
 
     print(f'Salario inicial: {salario_inicial:.2f} R$\n'
           f'Percentual de aumento aplicado: {percentual_aumento:.2f} %\n'
-          f'Valor do aumento: {valor_aumento:.2f} R$ \n'
-          f'Novo Salário: {salario_com_aumento:.2f} R$ ')
+          f'Valor do aumento: {valor_aumento:.2f} R$\n'
+          f'Novo Salário: {salario_com_aumento:.2f} R$')
 
 
-if __name__ == '__main__':
-    aumento_salarial()
+
